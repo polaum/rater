7a68b04bbe3eeeea5ea7e811fc63ef703262a135
Pola Umnitzin <polaumnitzin@MatanS-MBP.fritz.box>
2019-02-17 18:26:03 +0100

    Improving db

--- rater_func.py
+++ rater_func.py
@@ -13,32 +13,43 @@ users = users_db['users_collection']
 
 class User:
     def __init__(self, userid: int, limit=None):
-        user = users.find_one({'user_id': userid})
-        if user:
-            # user exists
-            self.consumption = user['consumption']
-            self.limit = user['limit']
-        else:
+        self.db_user_doc = users.find_one({'user_id': userid})
+        if not self.db_user_doc:
             # new user
             users.insert_one({'user_id': userid,'consumption': 0, 'limit': limit})
-            self.consumption = 0
-            self.limit = limit
-        self._userid = userid
+            self.db_user_doc = users.find_one({'user_id': userid})
 
     @property
     def userid(self):
-        return self._userid
+        return self.db_user_doc['user_id']
+
+    @property
+    def cons(self) -> int:
+        return int(self.db_user_doc['consumption'])
+
+    @property
+    def limit(self) -> int:
+        _value = self.db_user_doc['limit']
+        if _value:
+            return int(_value)
+
+    @cons.setter
+    def cons(self, value: int):
+        self.db_user_doc['consumption'] = self.cons + value
+        users.save(self.db_user_doc)
+
+    @limit.setter
+    def limit(self, value: int):
+        self.db_user_doc['limit'] = value
+        users.save(self.db_user_doc)
 
     def _can_consume(self, desired_added_cons=0):
         if not self.limit:
             return True
-        return self.limit >= (self.consumption + desired_added_cons)
+        return self.limit >= (self.cons + desired_added_cons)
 
     def consume(self, added_cons: int):
         if self._can_consume(added_cons):
-            self.consumption += added_cons
-            this_user = users.find_one({'user_id': self.userid})
-            this_user['consumption'] = self.consumption
-            users.save(this_user)
+            self.cons += added_cons
         else:
             raise ConsumptionException('User not allowed to consume this')
